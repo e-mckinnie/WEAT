533c5e19ae74a934dc4c82398c7ab2889a312d2a
e-mckinnie <elizabeth.mckinnie@outlook.com>
2021-10-12 23:04:55 -0700

    Create main.py
    add outline

---
+++ main.py
@@ -0,0 +1,26 @@
+
+def load_data(file_name):
+
+def embed_data(data):
+
+def effect_size(embeddings):
+
+def p_value(embeddings):
+
+
+
+def __init__ main():
+    file_name = ''
+    data = load_data(file_name)
+    embeddings = embed_data(data)
+
+    effect_size = effect_size(embeddings)
+    p_value = effect_size(embeddings)
+
+    print("effect_size: " + effect_size)
+    print("p_value:" + p_value)
+
+
+
+
+
