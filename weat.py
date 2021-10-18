f80675898805b3ee74e59d874110d4c6ff6cbbf1
e-mckinnie <elizabeth.mckinnie@outlook.com>
2021-10-17 17:30:23 -0700

    add write to file for relevant embeddings

--- weat.py
+++ weat.py
@@ -1,17 +1,19 @@
 import json
 import numpy as np
 import os
+import pdb
 
 
-# Load and prepare word embeddings
+# Load and prepare word embeddings that match target and attribute words
 def get_embeddings():
-    embeddings = {}
+    pdb.set_trace()
     f = open('glove.840B.300d.txt', encoding='utf8')
+    embeddings = {}
     for line in f:
         try:
-            line = line.split()
-            word = line[0]
-            embeddings[word] = line[1:]
+            line_array = line.split()
+            word = line_array[0]
+            embeddings[word] = line
         except Exception:
             pass
     return embeddings
@@ -29,7 +31,7 @@ def load_targets_and_attributes(file_name):
 def embed_data(data, embeddings_dict, embedded_data_file_name):
     embedded_data = {}
     for label in data:
-        words = data[label]["values"]
+        words = data[label]
         embedded_data[label] = []
         for i in words:
             embedded_data[label].append(_get_glove_embedding(i, embeddings_dict))
@@ -52,10 +54,11 @@ def load_embedded_data(embedded_data_file_name):
     with open(embedded_data_file_name, 'r') as file:
         data = json.load(file)
 
+    embeddings = {}
     for label in data:
-        data[label] = [np.asarray(embedding, type='float32') for embedding in data[label]]
+        embeddings[label] = [np.asarray(embedding.split()[1:], dtype='float32') for embedding in data[label]]
 
-    return data
+    return embeddings
 
 
 # Calculate effect size
@@ -82,16 +85,16 @@ def _cos(x, y):
 if __name__ == '__main__':
     file_name = 'flowers_insects_pleasant_unpleasant.json'
     embedded_data_file_name = 'flowers_insects_pleasant_unpleasant_embedded.json'
-    if not os.path.isfile(embedded_data_file_name):
-        glove_embeddings = get_embeddings()
 
+    if not os.path.isfile(embedded_data_file_name):
         data = load_targets_and_attributes(file_name)
+        glove_embeddings = get_embeddings()
         embed_data(data, glove_embeddings, embedded_data_file_name)
 
     embedded_data = load_embedded_data(embedded_data_file_name)
-    effect_size = effect_size(embedded_data['target_1'], embedded_data['target_2'], embedded_data['attribute_1'], embedded_data['attribute_2'])
+    d = effect_size(embedded_data['target_1'], embedded_data['target_2'], embedded_data['attribute_1'], embedded_data['attribute_2'])
     # p_value = effect_size(embeddings)
 
     print('file: ' + file_name)
-    print('effect_size: ' + str(effect_size))
+    print('effect_size: ' + str(d))
     # print("p_value:" + p_value)
